ffcd0f565067dc87eada13a61eeadba84d5a323f
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-04-05 01:25:25 +0200

    angularJS

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,5 @@
-/**
- * Created by riccardo on 05/04/15.
- */
+var gae_hopster_auth_frontend = angular.module('gae_hopster_auth_frontend',[]);
+
+gae_hopster_auth_frontend.controller('mainController', function($scope, $http){
+
+});
