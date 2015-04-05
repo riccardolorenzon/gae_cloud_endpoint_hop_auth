4b1ed73c77803ecb5fc5838555004195f10c744a
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-04-05 22:40:17 +0200

    add main controller

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,12 @@
 var gae_hopster_auth_frontend = angular.module('gae_hopster_auth_frontend',[]);
 
 gae_hopster_auth_frontend.controller('mainController', function($scope, $http){
-
+    $scope.read= function() {
+        message = {
+            "id_value": $scope.id_value
+        };
+    }
+    gapi.client.guestbook.messages.read_value(message).execute(function(resp){
+        $scope.read_output = 'read successfull';
+    });
 });
