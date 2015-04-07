be8ae4f98def34e4030e18184214da481f50a0f2
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-04-07 23:42:05 +0200

    oauth2 authentication

--- gae_api.py
+++ gae_api.py
@@ -6,6 +6,8 @@ from protorpc import remote
 from models.entity import Entity
 from google.appengine.ext import ndb
 
+WEB_CLIENT_ID = '70641680892-nkjkmbqbd9cmilcrg9sg7pbc6a9gch1a.apps.googleusercontent.com'
+
 class GaeRequest(messages.Message):
   id_value = messages.StringField(1)
   string_value = messages.StringField(2, required=True)
@@ -15,7 +17,9 @@ class GaeResponse(messages.Message):
   string_value = messages.StringField(2)
 
 @endpoints.api(name='readWriteApi',version='v1',
-               description='simple endpoint class with a read and write component')
+               allowed_client_ids=[WEB_CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID],
+               description='simple endpoint class with a read and write component',
+               scopes=[endpoints.EMAIL_SCOPE])
 class GaeApi(remote.Service):
     @endpoints.method(GaeRequest,
                   GaeResponse,
@@ -23,11 +27,15 @@ class GaeApi(remote.Service):
                   http_method='GET')
     def read_value(self, request):
         """
-        read component
+        Get the entity based on its string value(if multiple entities are found the first one is fetched).
+        If the entity does not exist it creates a new one and returns its string value and id.
         :param request:
         :return:
         """
         try:
+            current_user = endpoints.get_current_user()
+            if current_user is None:
+                raise endpoints.UnauthorizedException('User not authenticated.')
             gae_response = GaeResponse()
             # get the entity from the string
             if not request.string_value:
@@ -54,11 +62,16 @@ class GaeApi(remote.Service):
                   http_method='POST')
     def write_value(self, request):
         """
-        write component
+        Get the first entity with the provided string value, if the entity already exists in the datastore
+        (if multiple entities are found the first one is fetched), returns the string value and the entity's id.
+        Raise exception if entity does not exist.
         :param request:
         :return:
         """
         try:
+            current_user = endpoints.get_current_user()
+            if current_user is None:
+                raise endpoints.UnauthorizedException('User not authenticated.')
             gae_response = GaeResponse()
             # get the entity from the string
             if not request.string_value:
--- static/js/app.js
+++ static/js/app.js
@@ -5,53 +5,68 @@ function init() {
     }
 
 gae_hopster_auth_frontend_module.controller('mainController', function($scope, $window){
+    var ROOT = 'https://gae-hopster-auth.appspot.com/_ah/api/';
     $window.init= function() {
-      $scope.$apply($scope.load_readwrite_gae);
-    };
+        $scope.signin(true, $scope.userAuthed);
+        var apisToLoad;
+        var callback = function() {
+            if (--apisToLoad == 0) {
+              $scope.signin(true,
+                  $scope.userAuthed);
+            }
+          }
 
-    $scope.load_readwrite_gae = function() {
-        var ROOT = 'https://gae-hopster-auth.appspot.com/_ah/api/';
-        gapi.client.load('readWriteApi', 'v1', function() {
-            // TODO insert initialization values
-        }, ROOT);
-    };
+        apisToLoad = 1; // must match number of calls to gapi.client.load()
+        gapi.client.load('readWriteApi', 'v1', callback, ROOT);
+
+     };
+
+    $scope.signin = function(mode, authorizeCallback) {
+        gapi.auth.authorize({
+            client_id: '70641680892-nkjkmbqbd9cmilcrg9sg7pbc6a9gch1a.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/userinfo.email',
+            immediate: mode},
+        authorizeCallback);
+}
+    $scope.userAuthed = function(){
+        request =
+            gapi.client.oauth2.userinfo.get().execute(function(resp) {
+        if (!resp.code) {
+            // User is signed in, call my Endpoint
+            }
+        });
+    }
 
     $scope.read = function() {
-        var ROOT = 'https://gae-hopster-auth.appspot.com/_ah/api/';
-        gapi.client.load('readWriteApi', 'v1', function() {
-            if (typeof $scope.read_string_value == 'undefined' || $scope.read_string_value === '')
-            {
+        if (typeof $scope.read_string_value == 'undefined' || $scope.read_string_value === '') {
                 $window.alert('insert value for string value');
             }
-            message = {
+        var ROOT = 'https://gae-hopster-auth.appspot.com/_ah/api/';
+        message = {
                 "string_value": $scope.read_string_value
             };
-            gapi.client.readWriteApi.read(message).execute(function (resp) {
-                if (typeof resp.id_value === 'undefined')
-                    $window.alert('error on read, message: ' + resp.error.message)
-                else
-                    $window.alert('successful read, id: ' + resp.id_value);
-            });
-        }, ROOT);
+        gapi.client.readWriteApi.read(message).execute(function (resp) {
+            if (typeof resp.id_value === 'undefined')
+                $window.alert('error on read, message: ' + resp.error.message)
+            else
+                $window.alert('successful read, id: ' + resp.id_value);
+        });
     }
 
     $scope.write = function() {
-        var ROOT = 'https://gae-hopster-auth.appspot.com/_ah/api/';
-        gapi.client.load('readWriteApi', 'v1', function() {
-            if (typeof $scope.write_string_value == 'undefined' || $scope.write_string_value === '')
-            {
-                $window.alert('insert value for string value');
-            }
-            message = {
-                "string_value": $scope.write_string_value
-            };
-            gapi.client.readWriteApi.write(message).execute(function (resp) {
-                if (typeof resp.id_value === 'undefined')
-                    $window.alert('error on write, message: ' + resp.error.message)
-                else
-                    $window.alert('successful write, id: ' + resp.id_value);
-            });
-        }, ROOT);
+        if (typeof $scope.write_string_value == 'undefined' || $scope.write_string_value === '')
+        {
+            $window.alert('insert value for string value');
+        }
+        message = {
+            "string_value": $scope.write_string_value
+        };
+        gapi.client.readWriteApi.write(message).execute(function (resp) {
+            if (typeof resp.id_value === 'undefined')
+                $window.alert('error on write, message: ' + resp.error.message)
+            else
+                $window.alert('successful write, id: ' + resp.id_value);
+        });
 
     }
 });
